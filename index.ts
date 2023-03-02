d1b2fc60f21bdacf6ec6546aea2d4b7f32e1d948
DJ <damianjaroszek90@gmail.com>
2023-03-02 17:21:25 +0100

    Add error handling

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express, {json} from 'express';
 import cors from 'cors';
 import 'express-async-errors';
+import {handleError} from "./utils/error";
 
 const app = express();
 
@@ -8,6 +9,19 @@ app.use(cors({
     origin: 'http:/localhost:3000',
 }));
 app.use(json());
+
+// Routers
+
+// app.get('/', async(req, res)=>{
+//     throw new Error('Something was wrong');  // sprawdzamy czy obsługa błędów działa
+// });
+
+// app.get('/', async(req, res)=> {
+//     throw new ValidationError('Something was wrong');  // sprawdzamy czy obsługa błędów działa
+// });
+
+// Routers
+app.use(handleError);
 
 app.listen(3001, '0.0.0.0', () => {
     console.log('Listening on port http://localhost:3001');
