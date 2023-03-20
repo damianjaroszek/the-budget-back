796768356262342ba4c820790c6df3613b7db305
DJ <damianjaroszek90@gmail.com>
2023-03-20 12:10:06 +0100

    Added types RecipeEntity for Recipe

---
+++ types/recipe/recipe-entity.ts
@@ -0,0 +1,10 @@
+export interface RecipeEntity {
+    id: string;
+    date: string;
+    price: number;
+    name: string;
+    weight: number;
+    shopName: string;
+    category: string;
+    symbol: string;
+}
