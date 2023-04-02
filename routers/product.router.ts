4d9a4c5060f7d70dde59b3a14b49aee48d6a0c64
DJ <damianjaroszek90@gmail.com>
2023-04-02 21:46:06 +0200

    Added delete and post endpoints

--- routers/product.router.ts
+++ routers/product.router.ts
@@ -10,6 +10,17 @@ productRouter
         const products: ProductEntity[] = await ProductRecord.getAll();
         res.json(products);
     })
+    .delete('/:id', async (req, res) => {
+        const deleteProduct = await ProductRecord.deleteFromDb(req.params.id);
+        res.json(deleteProduct);
+        console.log(deleteProduct)
+    })
+    .post('/', async (req, res) => {
+        const addProduct = ProductRecord.insertToDb(req.body);
+        res.json(addProduct);
+        console.log(req.body);
+
+    });
 
 // .get('/:id', async (req, res) => {
 //     const ad: AdEntity = await AdRecord.getOne(req.params.id);
