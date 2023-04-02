f98e53cf2d04e94a14938a6c82ed16c52183c78f
DJ <damianjaroszek90@gmail.com>
2023-04-02 21:44:37 +0200

    Changed ProductEntity Interface, moved NewProductEntity

--- types/product/product-entity.ts
+++ types/product/product-entity.ts
@@ -1,7 +1,17 @@
 export interface ProductEntity {
     id?: string;
     name: string;
-    weight: number;
-    categoryName: string;
+    //weight: number;
+    categoryName?: string;
+    categoryId?: string;
     //symbol: string;
+    isDeletable: number;
 }
+
+export interface NewProductEntity extends Omit<ProductEntity, 'id'> {
+    id?: string;
+    categoryId?: string;
+}
+
+
+
