3aadf988e1cf445af659e10f52da7e588550ee88
DJ <damianjaroszek90@gmail.com>
2023-04-02 21:50:06 +0200

    Moved interface NewProductEntity to types, added insertToDb, deleteFromDb, fixed queries (remove weight and unit). Changed ProductRecord properties.

--- records/product.record.ts
+++ records/product.record.ts
@@ -1,20 +1,20 @@
-import {ProductEntity} from "../types";
+import {NewProductEntity, ProductEntity} from "../types";
 import {ValidationError} from "../utils/error";
 import {pool} from "../utils/db";
 import {FieldPacket} from "mysql2";
+import {v4 as uuid} from "uuid";
 
 type ProductRecordResult = [ProductEntity[], FieldPacket[]];
 
-interface NewProductEntity extends Omit<ProductEntity, 'id'> {
-    id?: string;
-}
 
 export class ProductRecord implements ProductEntity {
 
     id?: string;
     name: string;
-    weight: number;
+    //weight: number;
     categoryName: string;
+    categoryId?: string;
+    isDeletable: number;
 
     //symbol: string;
 
@@ -24,13 +24,9 @@ export class ProductRecord implements ProductEntity {
             throw new ValidationError('The name can not be empty or contains more than 50 chars.');
         }
 
-        if (obj.weight < 0.01 || obj.weight > 9999999.99) {
-            throw new ValidationError('The weight must be contain in scope 0.01 -  9 999 999.99.');
+        if (!obj.categoryName || obj.categoryName.length > 100) {
+            throw new ValidationError('The name of category can not be empty or contains more than 100 chars.');
         }
-
-        // if (!obj.categoryName || obj.categoryName.length > 100) {
-        //     throw new ValidationError('The name of category can not be empty or contains more than 100 chars.');
-        // }
 
         // if (!obj.symbol || obj.symbol.length > 4) {
         //     throw new ValidationError('The symbol of weight name can not be empty or contains more than 4 chars.');
@@ -38,8 +34,10 @@ export class ProductRecord implements ProductEntity {
 
         this.id = obj.id;
         this.name = obj.name;
-        this.weight = obj.weight;
-        //this.categoryName = obj.categoryName;
+        this.categoryId = obj.categoryId;
+        //this.weight = obj.weight;
+        this.categoryName = obj.categoryName;
+        this.isDeletable = obj.isDeletable;
         //this.symbol = obj.symbol;
 
 
@@ -47,11 +45,34 @@ export class ProductRecord implements ProductEntity {
 
 
     static async getAll(): Promise<ProductEntity[]> {
-        const [results] = await pool.execute('SELECT \n' +
-            '\t`index`.`id`, CONCAT(`index`.`name`, " ", ROUND(`index`.`weight`, 2), `unit`.`symbol`) AS name\n' +
+        const [results] = await pool.execute('SELECT `index`.`id`, `index`.`name`, `category`.`name` AS categoryName, `index`.`is_deletable` AS isDeletable\n' +
             'FROM `index`\n' +
-            'LEFT JOIN `unit` ON `index`.`unit`=`unit`.`id`') as ProductRecordResult;
+            'LEFT JOIN `category` ON `index`.`category` = `category`.`id`') as ProductRecordResult;
         return results.map(result => new ProductRecord(result));
+    }
+
+    static async deleteFromDb(id: string): Promise<ProductEntity[]> {
+        if (!id) {
+            throw new Error(`Product with ${id} is not exist in database.`)
+        }
+
+        const [affectedRows] = await pool.execute('DELETE FROM `the_budget`.`index` WHERE `index`.`id`=:id', {
+            id,
+        });
+        return affectedRows as ProductEntity[];
+    }
+
+    static async insertToDb(obj: NewProductEntity): Promise<string> {
+        if (!obj.id) {
+            obj.id = uuid();
+        } else {
+            throw new ValidationError('The Product is already exist.')
+        }
+        await pool.execute('INSERT INTO `the_budget`.`index` (`name`, `category`) VALUES (:name, :category)', {
+            name: obj.name,
+            category: obj.categoryId,
+        });
+        return obj.id;
     }
 
 
