ea3c94b7c8648a6a76e6f9da6717f007e5181074
DJ <damianjaroszek90@gmail.com>
2023-04-07 21:55:33 +0200

    Added delete and post endpoints in category.router.ts

--- routers/category.router.ts
+++ routers/category.router.ts
@@ -8,28 +8,16 @@ categoryRouter
     .get('/listAll', async (req, res) => {
         const category: CategoryEntity[] = await CategoryRecord.getAll();
         res.json(category);
+    })
+    .delete('/:id', async (req, res) => {
+        const deleteCategory = await CategoryRecord.deleteFromDb(req.params.id);
+        res.json(deleteCategory);
+    })
+    .post('/', async (req, res) => {
+        const addCategory = new CategoryRecord(req.body);
+        console.log(addCategory);
+        await addCategory.insertToDb();
+        res.json(addCategory);
+
     });
-// .delete('/:id', async (req, res) => {
-//     const deleteCategory = await ShopRecord.deleteFromDb(req.params.id);
-//     res.json(deleteCategory);
-// })
-// .post('/', async (req, res) => {
-//     const addCategory = new ShopRecord(req.body);
-//     console.log(addCategory);
-//     await addCategory.insertToDb();
-//     res.json(addCategory);
-//
-// });
 
-
-// .get('/:id', async (req, res) => {
-//     const ad: AdEntity = await AdRecord.getOne(req.params.id);
-//     res.json(ad);
-// })
-//
-// .post('/', async (req, res) => {
-//     const ad = new AdRecord(req.body);
-//     await ad.insert();
-//     res.json(ad);
-//
-// });
